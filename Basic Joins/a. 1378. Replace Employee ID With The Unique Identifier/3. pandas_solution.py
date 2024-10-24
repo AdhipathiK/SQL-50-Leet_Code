2cc83560110c3c81b4f7a84e2256d334767099a4
Adhipathi Kannan <151865143+AdhipathiK@users.noreply.github.com>
2024-10-24 17:29:22 +0100

    Update 3. pandas_solution.py

--- Basic Joins/a. 1378. Replace Employee ID With The Unique Identifier/3. pandas_solution.py
+++ Basic Joins/a. 1378. Replace Employee ID With The Unique Identifier/3. pandas_solution.py
@@ -1,10 +1,13 @@
 --Schema
-data = [[1, 'Will', None], [2, 'Jane', None], [3, 'Alex', 2], [4, 'Bill', None], [5, 'Zack', 1], [6, 'Mark', 2]]
-customer = pd.DataFrame(data, columns=['id', 'name', 'referee_id']).astype({'id':'Int64', 'name':'object', 'referee_id':'Int64'})
+data = [[1, 'Alice'], [7, 'Bob'], [11, 'Meir'], [90, 'Winston'], [3, 'Jonathan']]
+employees = pd.DataFrame(data, columns=['id', 'name']).astype({'id':'int64', 'name':'object'})
+data = [[3, 1], [11, 2], [90, 3]]
+employee_uni = pd.DataFrame(data, columns=['id', 'unique_id']).astype({'id':'int64', 'unique_id':'int64'})
 
 --Solution
 import pandas as pd
 
-def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
-    result = customer[(customer['referee_id'] != 2) | (customer['referee_id'].isnull())]
-    return result[['name']]
+def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+    result = pd.merge(employees, employee_uni, how='left', on='id')
+    return result[['unique_id','name']
+
