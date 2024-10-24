ea4eb34458411950e8da78ad71cbab53d1b8d789
Adhipathi Kannan <151865143+AdhipathiK@users.noreply.github.com>
2024-10-24 15:24:20 +0100

    Update 4. sql_solution.sql

--- Select/1148. Article Views/4. sql_solution.sql
+++ Select/1148. Article Views/4. sql_solution.sql
@@ -1,3 +1,4 @@
+--Schema
 Create table If Not Exists Views (article_id int, author_id int, viewer_id int, view_date date)
 Truncate table Views
 insert into Views (article_id, author_id, viewer_id, view_date) values ('1', '3', '5', '2019-08-01')
