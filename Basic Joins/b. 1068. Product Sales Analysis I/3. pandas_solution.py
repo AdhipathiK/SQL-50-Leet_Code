0804a17dbe1bb5cff98e89910440273ae650239b
Adhipathi Kannan <151865143+AdhipathiK@users.noreply.github.com>
2024-10-24 19:14:40 +0100

    Update 3. pandas_solution.py

--- Basic Joins/b. 1068. Product Sales Analysis I/3. pandas_solution.py
+++ Basic Joins/b. 1068. Product Sales Analysis I/3. pandas_solution.py
@@ -1,9 +1,12 @@
-data = [['Afghanistan', 'Asia', 652230, 25500100, 20343000000], ['Albania', 'Europe', 28748, 2831741, 12960000000], ['Algeria', 'Africa', 2381741, 37100000, 188681000000], ['Andorra', 'Europe', 468, 78115, 3712000000], ['Angola', 'Africa', 1246700, 20609294, 100990000000]]
-world = pd.DataFrame(data, columns=['name', 'continent', 'area', 'population', 'gdp']).astype({'name':'object', 'continent':'object', 'area':'Int64', 'population':'Int64', 'gdp':'Int64'})
+--Schema
+data = [[1, 100, 2008, 10, 5000], [2, 100, 2009, 12, 5000], [7, 200, 2011, 15, 9000]]
+sales = pd.DataFrame(data, columns=['sale_id', 'product_id', 'year', 'quantity', 'price']).astype({'sale_id':'Int64', 'product_id':'Int64', 'year':'Int64', 'quantity':'Int64', 'price':'Int64'})
+data = [[100, 'Nokia'], [200, 'Apple'], [300, 'Samsung']]
+product = pd.DataFrame(data, columns=['product_id', 'product_name']).astype({'product_id':'Int64', 'product_name':'object'})
 
 --Solution 
 import pandas as pd
 
-def big_countries(world: pd.DataFrame) -> pd.DataFrame:
-    result = world[(world['area'] >= 3000000) | (world['population'] >= 25000000)]
-    return result[['name', 'population', 'area']]
+def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
+    result = pd.merge(sales, product, how='left', on='product_id')
+    return result[['product_name', 'year', 'price']]
