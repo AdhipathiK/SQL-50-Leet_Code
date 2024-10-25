c91b2e9e8b3689f33522a19afb81a8c825df1626
Adhipathi Kannan <151865143+AdhipathiK@users.noreply.github.com>
2024-10-25 11:29:38 +0100

    Update 4. sql_solution.sql

--- Basic Joins/c. 1581. Customer Who Visited but Did Not Make Any Transactions/4. sql_solution.sql
+++ Basic Joins/c. 1581. Customer Who Visited but Did Not Make Any Transactions/4. sql_solution.sql
@@ -1,16 +1,25 @@
 --Schema
-Create table If Not Exists Views (article_id int, author_id int, viewer_id int, view_date date)
-Truncate table Views
-insert into Views (article_id, author_id, viewer_id, view_date) values ('1', '3', '5', '2019-08-01')
-insert into Views (article_id, author_id, viewer_id, view_date) values ('1', '3', '6', '2019-08-02')
-insert into Views (article_id, author_id, viewer_id, view_date) values ('2', '7', '7', '2019-08-01')
-insert into Views (article_id, author_id, viewer_id, view_date) values ('2', '7', '6', '2019-08-02')
-insert into Views (article_id, author_id, viewer_id, view_date) values ('4', '7', '1', '2019-07-22')
-insert into Views (article_id, author_id, viewer_id, view_date) values ('3', '4', '4', '2019-07-21')
-insert into Views (article_id, author_id, viewer_id, view_date) values ('3', '4', '4', '2019-07-21')
+Create table If Not Exists Visits(visit_id int, customer_id int)
+Create table If Not Exists Transactions(transaction_id int, visit_id int, amount int)
+Truncate table Visits
+insert into Visits (visit_id, customer_id) values ('1', '23')
+insert into Visits (visit_id, customer_id) values ('2', '9')
+insert into Visits (visit_id, customer_id) values ('4', '30')
+insert into Visits (visit_id, customer_id) values ('5', '54')
+insert into Visits (visit_id, customer_id) values ('6', '96')
+insert into Visits (visit_id, customer_id) values ('7', '54')
+insert into Visits (visit_id, customer_id) values ('8', '54')
+Truncate table Transactions
+insert into Transactions (transaction_id, visit_id, amount) values ('2', '5', '310')
+insert into Transactions (transaction_id, visit_id, amount) values ('3', '5', '300')
+insert into Transactions (transaction_id, visit_id, amount) values ('9', '5', '200')
+insert into Transactions (transaction_id, visit_id, amount) values ('12', '1', '910')
+insert into Transactions (transaction_id, visit_id, amount) values ('13', '2', '970')
 
 --Solution 1
-SELECT DISTINCT author_id AS id
-FROM Views
-WHERE author_id = viewer_id
-ORDER BY author_id ASC
+SELECT v.customer_id, COUNT(V.visit_id) AS count_no_trans
+FROM Visits v 
+LEFT JOIN Transactions t
+ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id
